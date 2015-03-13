a9a5b3c8497e9fb851f16819530a1cbd98151cee
Phil Weber <pweber@newrelic.com>
2015-03-13 16:11:17 -0700

    Fixed selector in tweets_controller. Changed formatting in tweets view to improve readability.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
     response = HTTParty.get('http://twitter.com/newrelic')
     
     parsed_data = Nokogiri::HTML.parse response.body
-    tweetNodes = parsed_data.xpath("//p[@class='js-tweet-text']")
+    tweetNodes = parsed_data.css(".js-tweet-text")
     @nodes = tweetNodes.collect do |node|
       node.inner_html
     end
