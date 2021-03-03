94d9a208473720afdb119e8c66b962fbcca4b5fb
Qryuu <qryuu.maoto@gmail.com>
2021-03-03 12:42:44 +0900

    Update 2.kata-install.sh

--- cloud9_amazon_linux2/Kata-Setup/2.kata-install.sh
+++ cloud9_amazon_linux2/Kata-Setup/2.kata-install.sh
@@ -5,7 +5,7 @@ curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/ec2-user/.rvm/scripts/rvm
 
-cd /home/ec2-user/newrelic-ruby-kata
+cd /home/ec2-user/environment/newrelic-ruby-kata
 
 rvm rvmrc warning ignore allGemfiles
 rvm install "ruby-2.2.2"
