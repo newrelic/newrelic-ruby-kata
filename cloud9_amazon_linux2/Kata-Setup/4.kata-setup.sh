6b2a25c28fb614b1e6968fd1cdd9f10e699d494a
Qryuu <qryuu.maoto@gmail.com>
2021-03-03 12:44:19 +0900

    Update 4.kata-setup.sh

--- cloud9_amazon_linux2/Kata-Setup/4.kata-setup.sh
+++ cloud9_amazon_linux2/Kata-Setup/4.kata-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ec2-user/newrelic-ruby-kata
+cd /home/ec2-user/environment/newrelic-ruby-kata
 
 bundle exec rake db:create
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER -d newrelic-ruby-kata_development public/sample-data.dump
