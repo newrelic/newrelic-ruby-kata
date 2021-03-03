b22b9dbc8f61f6b11d942ab1abab7fbf173a91b0
Qryuu <qryuu.maoto@gmail.com>
2021-03-03 12:42:24 +0900

    Update 3.ruby-install.sh

--- cloud9_amazon_linux2/Kata-Setup/3.ruby-install.sh
+++ cloud9_amazon_linux2/Kata-Setup/3.ruby-install.sh
@@ -2,7 +2,7 @@
 
 source /home/ec2-user/.rvm/scripts/rvm
 
-cd /home/ec2-user/newrelic-ruby-kata
+cd /home/ec2-user/environment/newrelic-ruby-kata
 
 rvm rvmrc warning ignore allGemfiles
 rvm install "ruby-2.2.2"
